f2ddef1c39f808b3b81c80b9e9febdfbaa2a42af
songhuijun <1129988319@qq.com>
2020-12-11 10:53:33 +0800

    内容列表样式开发

--- src/components/Common/ArticleList/ArticleList.js
+++ src/components/Common/ArticleList/ArticleList.js
@@ -1,12 +1,17 @@
 import React, {Component} from "react";
 import "./article-list.css";
 import {CloseOutlined} from "@ant-design/icons";
+import { Avatar } from 'antd';
 
 export default class ArticleList extends Component {
   constructor(props) {
     super(props);
     this.state = {};
     // console.log(props);
+  }
+
+  closeArticle = (item) => {
+    console.log(item);
   }
 
   render() {
@@ -37,11 +42,44 @@ export default class ArticleList extends Component {
                 <span className="article-item-company">{articleData.company}</span>
                 <span className="article-item-comment">{articleData.comment}评论</span>
                 <span className="article-item-time">{articleData.time}</span>
-                <span className="article-item-close"><CloseOutlined/></span>
+                <span className="article-item-close" onClick={()=>this.closeArticle(articleData)}><CloseOutlined/></span>
               </div>
             </div>
             <div className="article-item-right">
-              <img alt="此处为图片" src="./images/image1.jpg"/>
+              <img alt="此处为图片" src={'./images/image' + articleData.imageList[0].index + '.jpg'}/>
+            </div>
+          </div>
+        }
+        {
+          articleData.type === "has-avatar" &&
+          <div className="article-item">
+            <div className="article-item-avatar">
+              <div>
+                <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+              </div>
+              <div>
+                <div>
+                  <b>{articleData.avatarInfo.title}</b>
+                  <span className="article-item-close" onClick={()=>this.closeArticle(articleData)}><CloseOutlined/></span>
+                </div>
+                <div>
+                  <span className="article-item-comment">{articleData.avatarInfo.fans}粉丝</span>
+                  <span className="article-item-time">{articleData.avatarInfo.company}</span>
+                </div>
+              </div>
+            </div>
+            <div className="article-item-title">
+              {articleData.title}
+            </div>
+            <div className="article-item-image">
+              {
+                articleData.imageList.map(
+                  (image, index) => (
+                    <div key={index}>
+                      <img alt="此处为图片" src={'./images/image' + image.index + '.jpg'}/>
+                    </div>)
+                )
+              }
             </div>
           </div>
         }
@@ -65,7 +103,7 @@ export default class ArticleList extends Component {
               <span className="article-item-company">{articleData.company}</span>
               <span className="article-item-comment">{articleData.comment}评论</span>
               <span className="article-item-time">{articleData.time}</span>
-              <span className="article-item-close"><CloseOutlined/></span>
+              <span className="article-item-close" onClick={()=>this.closeArticle(articleData)}><CloseOutlined/></span>
             </div>
           </div>
         }
